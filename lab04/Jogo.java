eeb85e5b2ee0fd38238b651a36b418b7bf9607f7
Ireneraquel <ireneraquelmc@gmail.com>
2024-07-21 21:44:17 +0000

    Jogo terminado

--- lab04/Jogo.java
+++ lab04/Jogo.java
@@ -1,5 +1,7 @@
 package lab04;
+
 import java.util.Scanner;
+
 public class Jogo {
     // java lab04/Jogo.java
     public static void main(String[] args) {
@@ -16,18 +18,18 @@ public class Jogo {
         System.out.println("Bem-vindo ao jogo!");
 
         while (num != usu_n) {
-            System.out.println("Digite um número?");
+            System.out.print("Digite um número? ");
             usu_n = sc.nextInt();
-            ten = + 1;
-            if (usu_n > num){
-                System.out.println("O número sorteado é maior");
-            }   else if {
-                    System.out.println("O número sorteado é menor");
-                } 
-                    else{} 
+            ten ++;
+            if (num > usu_n) {
+                System.out.println("O número sorteado é maior"); 
+            } else if(num < usu_n) {
+                System.out.println("O número sorteado é menor");  
+            } else {
+                System.out.println("Parabéns! Você acertou o número! ");
+                System.out.println("Número de tentativas que você realizou foi: "+ ten);
+            }
         }
-        
-        System.out.println("O número de tentativas foi: " + ten);
 
     }   
 }
